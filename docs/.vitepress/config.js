7b54aad075c8b537382b30648c521429cbff3acf
jobn123 <jobn_joy@163.com>
2022-06-09 21:39:03 +0800

    chore: add navigator

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -24,7 +24,9 @@ export default {
           { text: '掌握Vim语法', link: '/day3' },
           { text: '更有效率的处理单字符', link: '/day4' },
           { text: '掌握可视化模式', link: '/day5' },
-          { text: '掌握文本对象', link: '/day6' }
+          { text: '掌握文本对象', link: '/day6' },
+          { text: '在文件中更快的移动', link: '/day7' },
+          { text: '掌握搜索', link: '/day8' }
         ]
       },
       {
